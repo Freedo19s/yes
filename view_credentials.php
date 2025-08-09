ad14815a3aa9f13b1fdf155b438ddf7abe3bacfa
Freedo19s <freedoel579@gmail.com>
2025-08-09 12:36:34 +0100

    Update view_credentials.php
    Optional

--- view_credentials.php
+++ view_credentials.php
@@ -1,17 +1,16 @@
 <?php
-// Mot de passe secret pour éviter que n'importe qui y accède
-$secret = "cccc"; // Change ceci
+$secret_key = "cccc"; // clé secrète
 
-if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
+if (!isset($_GET['key']) || $_GET['key'] !== $secret_key) {
     http_response_code(403);
-    die("Accès refusé.");
+    die("Accès refusé");
 }
 
-$filename = "credentials.txt";
-if (file_exists($filename)) {
-    header("Content-Type: text/plain");
-    echo file_get_contents($filename);
-} else {
-    echo "Fichier introuvable.";
+$file = "credentials.txt";
+
+if (!file_exists($file)) {
+    die("Fichier introuvable");
 }
+
+echo "<pre>" . htmlspecialchars(file_get_contents($file)) . "</pre>";
 ?>
